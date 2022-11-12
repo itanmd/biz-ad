c3e15ed2c5d693c1020adecca3f8138f83577df8
Itan <itanmoshe@gmail.com>
2022-11-13 00:15:55 +0200

    header

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -2,11 +2,11 @@
 import react from "react"
 function Header() {
     return (
-      <nav className="navbar navbar-dark bg-dark">
+      <nav className="navbar navbar-dark bg-primary">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
-            <i className="bi-cup-hot me-3"></i>
-            Meal On Time
+            <i className="bi bi-shop me-3"></i>
+            BizAd
           </a>
         </div>
       </nav>
