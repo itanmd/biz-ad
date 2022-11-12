99f5dd60e9752846f38559d3b3b45c32b77058dc
Itan <itanmoshe@gmail.com>
2022-11-12 22:47:31 +0200

    bootstrap

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,1 +1,17 @@
+
 import react from "react"
+function Header() {
+    return (
+      <nav className="navbar navbar-dark bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            <i className="bi-cup-hot me-3"></i>
+            Meal On Time
+          </a>
+        </div>
+      </nav>
+    );
+  }
+  
+  export default Header;
+  
