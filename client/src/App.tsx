b4dc20882de305ee413a82211a14720c8a43be07
Itan <itanmoshe@gmail.com>
2022-12-07 09:29:56 +0200

    cards

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,30 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from "./components/Menu/Menu"
+// import Card from './components/Card/Card';
 
 function App() {
   return (
    <>
    <Header />
-   
+   <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+   </BrowserRouter>
+   {/* <div>
+    <Card name="Play Ground"
+    description= "Playground for children"
+    street="Some Street 5, Tel Aviv" 
+    imageUrl="https://cdn.pixabay.com/photo/2013/04/02/19/54/playground-99509_960_720.jpg"
+    website="https://www.toplinerec.com/"
+    phone="03-5550000"
+    />
+   </div> */}
+
    </>
   );
 }
---
+++ client/src/components/Menu/Menu.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+// import Card, { CardType } from "../Card/Card";
+
+// interface HomeProps {
+    
+// }
+ 
+// interface HomeState {
+//     cards: Array<CardType>
+// }
+ 
+// class Home extends React.Component<HomeProps, HomeState> {
+    
+//     render() { 
+//         return (  );
+//     }
+// }
+ 
+export default Menu;
