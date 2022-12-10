7d54ebf8cee9853d888fb2e4e5849000b6f53c80
Itan <itanmoshe@gmail.com>
2022-12-10 22:57:32 +0200

    images

--- client/src/components/About/About.tsx
+++ client/src/components/About/About.tsx
@@ -11,7 +11,7 @@ function About() {
 
             <div className="about">
                 <p className="aboutP">
-                Everyday many business owners choose to use our app to advertise their business and to gain more traction to their website and online store
+                Everyday many business owners choose to use our app to advertise their business and to gain more attention to their website and online store
                 </p>
                 <Link className="btn btn-primary btn-lg" to={"/"}>Start Today</Link>
             </div>
